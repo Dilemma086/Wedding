db6a30aa5df9b2df45c579fd515c27e9f8af313f
Dilemma086 <Dilemma086@yandex.ru>
2023-11-06 21:06:33 +0300

    remove file

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-
-let http = new XMLHttpRequest()
-http.open('get', 'data.json', true)
-
-http.send()
-
-
-http.onload = function renderData(){
-    let data = JSON.parse(this.responseText)
-        // const wedding = new ScreenOne()
-        wedding.creat()
-        wedding.append()
+function timer() {
+    const date = new Date(2023, 11, 30, 12)
+    const day = document.querySelector('.day')
+    const hour = document.querySelector('.hour')
+    const min = document.querySelector('.min')
+    const sec = document.querySelector('.sec')
+    var nowDate = new Date();
+    var achiveDate = new Date(2023, 11, 30, 12,0,0); //Задаем дату, к которой будет осуществляться обратный отсчет
+    var result = (achiveDate - nowDate)+1000;
+    var seconds = Math.floor((result/1000)%60);
+    var minutes = Math.floor((result/1000/60)%60);
+    var hours = Math.floor((result/1000/60/60)%24);
+    var days = Math.floor(result/1000/60/60/24);
+    if (seconds < 10) seconds = '0' + seconds;
+    if (minutes < 10) minutes = '0' + minutes;
+    if (hours < 10) hours = '0' + hours;
+    day.innerHTML = days
+    hour.innerHTML = hours
+    min.innerHTML = minutes
+    sec.innerHTML = seconds
+    setTimeout(timer, 1000);
 }
+timer();
 
-const main = document.querySelector('main')
-
-class ScreenOne{
-    elem = document.createElement('section')
-
-    constructor(){
-
-    }
-
-    creat(){
-        this.elem.classList.add('screen-one')
-        
-        this.elem.innerHTML = `
-            <div class="wedding-one">
-                <h1>Дорогие друзья, родные и близкие!</h1>
-                <p>В зимний день 30 декабря 2023 года, мы, Юлия и Евгений приглашаем разделить с нами знаменательное событие - рождение нашей семьи.</p>
-
-            </div>
-        `
-    }
-
-    append(){
-        main.append(this.elem)
-    }
-}
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+    //    change.target.classList.add('element-show');
+    console.log(change.target)
+      }
+    });
+  }
+  
+  let options = {threshold: [0.5]};
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('section');
+  console.log(elements)
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
